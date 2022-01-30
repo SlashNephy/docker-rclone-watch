5c7e1a66d2b7c2ce0a26d50edffedbbc2c5f8d7a
Nep <spica@starry.blue>
2022-01-30 14:52:06 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ fi
 # https://github.com/rclone/rclone/issues/249#issuecomment-425352531
 while [[ true ]] ; do
     # performs synchronizations / copy
-    `rclone $RCLONE_COMMAND`
+    eval "rclone $RCLONE_COMMAND"
 
     # waiting for something to change or it will pass 300 seconds
     inotifywait --recursive --timeout 300 -e modify,delete,create,move $RCLONE_DIR

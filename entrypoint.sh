36ba69b26d917bd7e7934dd9488735571d84e571
Nep <spica@starry.blue>
2022-01-30 14:49:03 +0900

    fix: indentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,8 @@ fi
 # https://github.com/rclone/rclone/issues/249#issuecomment-425352531
 while [[ true ]] ; do
     # performs synchronizations / copy
-	`rclone $RCLONE_COMMAND`
+    `rclone $RCLONE_COMMAND`
 
-	# waiting for something to change or it will pass 300 seconds
-	inotifywait --recursive --timeout 300 -e modify,delete,create,move $RCLONE_DIR
-
-	# going back to the beginning
+    # waiting for something to change or it will pass 300 seconds
+    inotifywait --recursive --timeout 300 -e modify,delete,create,move $RCLONE_DIR
 done

c42d35e92b51820a88d44aabe537f7b860775173
Nep <spica@starry.blue>
2022-01-30 14:34:16 +0900

    +x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/ash -ex
+#!/bin/ash -e
 
 if [ -z "$RCLONE_COMMAND" ]; then
-    echo "ERROR: RCLONE_COMMAND is not set" 1>&2
+    echo "ERROR: RCLONE_COMMAND is not set." 1>&2
     exit 1
 fi
 
@@ -13,7 +13,7 @@ fi
 # https://github.com/rclone/rclone/issues/249#issuecomment-425352531
 while [[ true ]] ; do
     # performs synchronizations / copy
-	rclone $RCLONE_COMMAND
+	`rclone $RCLONE_COMMAND`
 
 	# waiting for something to change or it will pass 300 seconds
 	inotifywait --recursive --timeout 300 -e modify,delete,create,move $RCLONE_DIR
